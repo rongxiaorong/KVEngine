67f2769b444fe1b8cc2e8a16f18ca5e35b32a623
hanmc <hanmingcong123@hotmail.com>
2018-11-07 10:33:54 +0800

    fzy

--- engine_race/BloomFilter.cc
+++ engine_race/BloomFilter.cc
@@ -1,54 +1,94 @@
-// BloomFilter.cc
 #include "include/BloomFilter.h"
 #include <memory.h>
 
-namespace polar_race {
-    BloomFilter::BloomFilter(int entry_size  /* m */, int entry_inserts  /* n */) {
-    	this->init(entry_size,entry_inserts);
+
+namespace polar_race
+{
+	unsigned char* BloomFilter::bitArray(unsigned int array_size)
+	{
+		array_size = array_size / blockLen + (array_size % blockLen > 0 ? 1:0);
+		unsigned char* bits = new unsigned char[array_size];
+		memset(bits, sizeof(unsigned char) * array_size, 0);
+		return bits;
 	}
-	void BloomFilter::init(int entry_size  /* m */, int entry_inserts  /* n */) {
-		// k = ln2 * (m/n)
+	
+	bool BloomFilter::getBit(unsigned char* bits, unsigned int index)
+	{
+		unsigned char data = bits[index / blockLen];
+		unsigned char pos = index % blockLen;
+		return ((data << pos) >> (blockLen - 1));
+	}
+	
+	void BloomFilter::setBit(unsigned char* bits, unsigned int index, unsigned char value)
+	{
+		unsigned char data = bits[index / blockLen];
+		unsigned char pos = index % blockLen;
+		unsigned char x = 0x80 >> pos;
+		if(!value)
+		{
+			x = ~x;
+			data &= x;
+		}
+		else
+			data |= x;
+			
+		bits[index / blockLen] = data;
+	}
+	
+    BloomFilter::BloomFilter(int entry_size  /* m */, int entry_inserts  /* n */)
+    {
+    	// k = ln2 * (m/n)
     	this->bitsetSize = entry_size;
 		this->sumHashFunction = (int)((double)entry_size / (double)entry_inserts * ln2);
 		this->sumHashFunction %= maxFunctions;
-		bool* temp = new bool[this->bitsetSize];
-		memset(temp, false, sizeof(bool) * this->bitsetSize);  // set as 0
-		this->dataPointer = temp;
+		//bool* temp = new bool[this->bitsetSize];
+		//memset(temp, false, sizeof(bool) * this->bitsetSize);  // set as 0
+		this->dataPointer = bitArray(this->bitsetSize);
+		//this->dataPointer = temp;
+		
 	}
 
-    void BloomFilter::set(const std::string src) {
+    void BloomFilter::set(const std::string src)
+    {
     	std::bitset<inputSize / 2> left(0), right(0);
     	this->preProcess(&left, &right, src);
 		for(int i = 1; i <= this->sumHashFunction; ++i)
 		{
 			int pos = this->dynamicHash(&left, &right, i);
-			this->dataPointer[pos] = true;
+			//this->dataPointer[pos] = true;
+			setBit(this->dataPointer, pos, 1);
 		}
 	}
     
-    bool BloomFilter::get(const std::string src) {
+    bool BloomFilter::get(const std::string src)
+    {
     	std::bitset<inputSize / 2> left(0), right(0);
     	this->preProcess(&left, &right, src);
 		for(unsigned int i = 1; i <= this->sumHashFunction; ++i)
 		{
 			unsigned int pos = this->dynamicHash(&left, &right, i);
-			if(!(this->dataPointer[pos]))
+			//if(!(this->dataPointer[pos]))
+				//return false;
+			if(!getBit(this->dataPointer, pos))
 				return false;
 		}
 		return true;
 	}
 
-    bool* BloomFilter::data() {
+    unsigned char* BloomFilter::data()
+    {
 		return this->dataPointer;
 	
 	}
 	
-	unsigned int BloomFilter::size() {
+	unsigned int BloomFilter::size()
+	{
 		return this->bitsetSize;
 		
 	}
 	
-	unsigned int BloomFilter::dynamicHash(std::bitset<inputSize / 2>* left, std::bitset<inputSize / 2>* right,  unsigned int salt) {
+	unsigned int BloomFilter::dynamicHash(std::bitset<inputSize / 2>* left, std::bitset<inputSize / 2>* right,  unsigned int salt)
+	{
 		unsigned int a = left->to_ulong();
 		unsigned int b = right->to_ulong();
 		// add salt--> extend
@@ -57,13 +97,15 @@ namespace polar_race {
 		
 	}
 	
-	void BloomFilter::preProcess(std::bitset<inputSize / 2>* left, std::bitset<inputSize / 2>* right, const std::string src) {
+	void BloomFilter::preProcess(std::bitset<inputSize / 2>* left, std::bitset<inputSize / 2>* right, const std::string src)
+	{
 		// to 8B
     	int length = inputSize / 8;  // number of bytes
     	if(src.size() < length)
     		length = src.size();
     	memcpy(left, src.c_str(), length / 2);
     	memcpy(right, src.c_str() + length / 2, length / 2 + length % 2);
+
 	}
 
-} // namespace polar_race
+}
--- engine_race/include/BloomFilter.h
+++ engine_race/include/BloomFilter.h
@@ -3,35 +3,35 @@
 #define _ENGINE_RACE_BLOOMFILTER_H_
 
 #include <string>
-#include <bitset>
-
-const int inputSize = 64;  // 8B
-const double ln2 = 0.693147;
-const int maxFunctions = 100;  // changeable 
+#include <bitset> 
 
 
 namespace polar_race {
 class BloomFilter {
 public:
     // size (bits) for an entry
-    BloomFilter(){};
-
     BloomFilter(int entry_size  /* m */, int entry_inserts  /* n */);
 
-    void init(int entry_size  /* m */, int entry_inserts  /* n */);
-    
     void set(const std::string src);
     
     bool get(const std::string src);
 
-    bool* data();
+    unsigned char* data();
 
     // size of the array (bytes)
     unsigned int size();
 private:
-	bool* dataPointer;
+	static const int inputSize = 64;  // 8B
+	static const constexpr double ln2 = 0.693147;
+	static const int maxFunctions = 100;  // changeable
+	static const unsigned int blockLen = 8;
+	unsigned char* dataPointer;
 	unsigned int bitsetSize;  // actually, it is a bool array
 	int sumHashFunction;
+	// dynamic_bitset ops
+	unsigned char* bitArray(unsigned int array_size);
+	bool getBit(unsigned char* bits, unsigned int index);
+	void setBit(unsigned char* bits, unsigned int index, unsigned char value);
 	
 	unsigned int dynamicHash(std::bitset<inputSize / 2>* left, std::bitset<inputSize / 2>* right,  unsigned int salt);
 	
